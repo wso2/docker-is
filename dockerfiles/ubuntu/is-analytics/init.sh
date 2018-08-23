78a29af8760794be8a928ea5edad63f466804bb7
chirangaalwis <chirangaalwis@gmail.com>
2018-08-24 01:32:51 +0530

    Improve Docker comments in Docker resources

--- dockerfiles/ubuntu/is-analytics/init.sh
+++ dockerfiles/ubuntu/is-analytics/init.sh
@@ -36,13 +36,12 @@ test ! -d ${WSO2_SERVER_HOME} && echo "WSO2 Docker product home does not exist" 
 test -d ${artifact_volume}/ && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
 test -d ${config_volume}/ && cp -RL ${config_volume}/* ${WSO2_SERVER_HOME}/
 
-# make any runtime or node specific configuration changes
-# for example, setting container IP in relevant configuration files
+# make any node specific configuration changes
 
 # set the Docker container IP as the `localMemberHost` under axis2.xml clustering configurations (effective only when clustering is enabled)
 sed -i "s#<parameter\ name=\"localMemberHost\".*<\/parameter>#<parameter\ name=\"localMemberHost\">${docker_container_ip}<\/parameter>#" ${WSO2_SERVER_HOME}/repository/conf/axis2/axis2.xml
 # replace host name entries (hard-coded with `wso2is-with-analytics-is-analytics`), with the Docker container IP in event-processor.xml file
 sed -i "s#<hostName>wso2is-with-analytics-is-analytics</hostName>#<hostName>${docker_container_ip}</hostName>#" ${WSO2_SERVER_HOME}/repository/conf/event-processor.xml
 
-# start the WSO2 Carbon server
+# start WSO2 Carbon server
 sh ${WSO2_SERVER_HOME}/bin/wso2server.sh
