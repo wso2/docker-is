cd0b5bda67204d4a7e7aad4118b59d171543f6a8
Hasintha Indrajee <hasinthaindrajee@gmail.com>
2020-03-10 18:18:29 +0530

    Update docker-entrypoint.sh

--- dockerfiles/ubuntu/is/docker-entrypoint.sh
+++ dockerfiles/ubuntu/is/docker-entrypoint.sh
@@ -32,6 +32,6 @@ test -d ${config_volume} && [ "$(ls -A ${config_volume})" ] && cp -RL ${config_v
 # copy any artifact changes mounted to artifact_volume
 test -d ${artifact_volume} && [ "$(ls -A ${artifact_volume})" ] && cp -RL ${artifact_volume}/* ${WSO2_SERVER_HOME}/
 
-cp /tmp/password-persist ${WSO2_SERVER_HOME}/
+#cp /tmp/password-persist ${WSO2_SERVER_HOME}/
 # start WSO2 Carbon server
 sh ${WSO2_SERVER_HOME}/bin/wso2server.sh "$@"
